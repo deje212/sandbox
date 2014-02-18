000908fc29b39f2bcc1a6d5dc51ca52ae31d80f1
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-02-18 17:56:05 -0300

    More useful macros

--- src/common.h
+++ src/common.h
@@ -1,6 +1,8 @@
 #ifndef __COMMON_INCLUDED__
 #define __COMMON_INCLUDED__
 
+#include <malloc.h>
+
 /* Macete: Caso uma função tenha parâmetros e
    um ou mais desses parâmetros não sejam usados,
    usar essa macro evita que o compilador emita
@@ -9,5 +11,13 @@
 
 /* Macete: Obtem o número de itens de um array. */
 #define ARRAY_ITEMS(array) ( sizeof((array)) / sizeof(&(array)[0]) )
+
+/* Testa se um dos bits em 'bits' estão setados em 'x'. */
+#define MASK_BITS(x,bits) ((x) & (bits))
+#define TEST_BITS(x,bits) ((MASK_BITS((x),(bits))) != 0)
+
+/* NEW() e DELETE() C++ style for C (be careful!) */
+#define NEW(type, n) ( (type *)malloc(sizeof(type)*(n)) )
+#define DELETE(p) { free((p)); }
 
 #endif
