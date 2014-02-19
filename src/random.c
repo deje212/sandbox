a7409169cd399717a5ec14a19b1765db75c02e44
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-02-19 10:22:46 -0300

    /dev/urandom never blocks or deplete.

--- src/random.c
+++ src/random.c
@@ -6,23 +6,20 @@
 /* Retorna -1 se erro, 0 se ok */
 int read_random_array(void *data, size_t size)
 {
-  int fd;
-  size_t len;
-  long int *p;
+  int fd, ret;
+
+  if (size == 0)
+    return -1;
 
   fd = open("/dev/urandom", O_RDONLY);
   if (fd < 0)
     return -1;
 
-  p = data;
-  for (len = 0; len < size; len++)
-    if (read(fd, p++, sizeof(long int)) < 0)
-    {
-      close(fd);
-      return -1;
-    }
+  ret = 0;
+  if (read(fd, data, size) < 0)
+    ret = -1;
 
-  close(fd);
+  fclose(fd);
 
-  return 0;  
+  return ret;
 }
