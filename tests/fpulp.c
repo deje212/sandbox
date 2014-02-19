4f1b178af6ab74f87c635f0586e7452021a61ba7
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-02-19 13:18:08 -0300

    Comparação por ULP's

---
+++ tests/fpulp.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdlib.h>
+
+typedef union {
+  float f;
+  unsigned int i;
+  struct {
+    unsigned int mantissa:23;
+    unsigned int expoent:8;
+    unsigned int sign:1;
+  } s;
+} f32_t;
+
+/* Compara os floats como se fossem inteiros. 
+   Não leva em consideração NaN's e infinitos. */
+int compare_floats2(float x, float y)
+{
+  /* Verifica o sinal */
+  if (((f32_t *)&x)->s.sign != ((f32_t *)&y)->s.sign)
+  {
+    /* Cerca a possibilidade de compara +0 com -0. */
+    if (x == y)
+      return 1;
+    return 0;
+  }
+
+  return abs(((f32_t *)&x)->i - ((f32_t *)&y)->i) <= 1;
+}
