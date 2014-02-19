638662f90e7141afec853f5297dd43325f738398
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-02-19 09:48:12 -0300

    Pseudo random generators & tips

---
+++ src/random.h
@@ -0,0 +1,23 @@
+#ifndef __RANDOM_INCLUDED__
+#define __RANDOM_INCLUDED__
+
+#include <stdlib.h>
+
+/*
+  Um fato que me foi apontado por Nelson Brito, desenvolvedor do T50,
+  é que os bits menos significativos de um gerador de números pseudo
+  aleatórios, baseado numa equação linear, são "pouco aleatórios".
+
+  Assim, ao tentar obter valores aleatórios de 8, 16 ou 32 bits, através
+  de uma simples máscara, usando AND, pode nos dar a falsa ilusão de
+  aleatoriedade... Para "garantir" maior aleatoriedade é conveniente
+  usar os bits superiores...
+*/
+
+#define _RND_UPPER_BITS(x,bits) ((x) >> (sizeof(long int) - (bits))
+
+#define _CHAR_RND(x)  _RND_UPPER_BITS((x),sizeof(char))
+#define _SHORT_RND(x) _RND_UPPER_BITS((x),sizeof(short))
+#define _INT_RND(x)   _RND_UPPER_BITS((x),sizeof(int))
+
+#endif
