1288d97f0fd47763e24e3f0b3c4574c592d1213a
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-04-08 16:28:14 -0300

    Some comments.

--- tests/jni-helloworld/libHello.c
+++ tests/jni-helloworld/libHello.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <jni.h>
 
+/* 
+   ATENÇÃO: O nome da função exportada tem que seguir o padrão:
+
+            Java_#class_#function.
+
+            Onde: #class é exatamente o nome da classe.
+                  #function é exatamente o nome da função nativa.
+
+            A documentação do JNI explica a nomenclatura... (é bom ver!).
+
+            A lista de parâmetros sempre tem que ser:
+            (JNIEnv *env, jobject obj, ...)
+
+            Onde os parâmetros adicionais (depois de 'obj') são
+            dos tipos definidos em jni.h.
+
+            Veja HelloWorld.java. 
+*/
 JNIEXPORT void JNICALL Java_HelloWorld_print(JNIEnv *env, jobject obj)
 {
   printf("Hello, world!\n");
