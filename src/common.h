2904936c83b9a63c3e929265faed1e47803f3f0f
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-04-10 16:37:14 -0300

    Added some string routines

--- src/common.h
+++ src/common.h
@@ -25,8 +25,8 @@
 
 /* COMPARE returna 1 para "maior que", -1 para "menor que" e 0 para "igual a".
 
-   WARNING: Usar essas macros pode ser perigoso!
-	 Acho que a especificação da linguagem C dita que qualquer valor diferente
+   AVISO: Usar essas macros pode ser perigoso!
+	 A especificação da linguagem C dita que qualquer valor diferente
    de zero é TRUE. As macros abaixo supõem que TRUE é 1.
 
 	 Na arquitetura Intel (x86 e x86_64) isso parece ser verdadeiro, já que
@@ -34,7 +34,6 @@
    para denotar TRUE. 
 
 	 Está dado o aviso! */
-
 #define COMPARE(x,y) (((x) > (y)) - ((x) < (y)))
 #define SIGN(x) COMPARE((x), 0)
 
