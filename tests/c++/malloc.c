222433860e26def667342a4e6170f9e45b6815b6
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-05-17 14:27:57 -0300

    Tests

--- tests/c++/malloc.c
+++ tests/c++/malloc.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <malloc.h>
-#include "cycle-counting.h"
+#include "../../src/cycle-counting.h"
 
 int main(void)
 {
-  long long t;
+  long long t1, t2;
   char *p[1000];
   int i;
 
-  START_CYCLE_COUNT(t);
+  START_CYCLE_COUNT(t1);
   for (i = 0; i < 1000; i++)
     p[i] = malloc(1000);
-  STOP_CYCLE_COUNT(t);
+  STOP_CYCLE_COUNT(t2);
 
-  printf("%lld ciclos de máquina.\n", t / 1000);
+  printf("%lld ciclos de máquina.\n", (t2 - t1) / 1000);
 }
--- tests/c++/new.cc
+++ tests/c++/new.cc
@@ -1,19 +1,19 @@
 #include <iostream>
-#include "cycle-counting.h"
+#include "../../src/cycle-counting.h"
 
 using namespace std;
 
 int main(void)
 {
-  long long t;
+  long long t1, t2;
   char *p[1000];
 
-  START_CYCLE_COUNT(t);
+  START_CYCLE_COUNT(t1);
   for (int i = 0; i < 1000; i++)
     p[i] = new char [1000];
-  STOP_CYCLE_COUNT(t);
+  STOP_CYCLE_COUNT(t2);
 
-  cout << (t / 1000) << " ciclos de máquina.\n";
+  cout << ((t2 - t1) / 1000) << " ciclos de máquina.\n";
 
   return 0;
 }
