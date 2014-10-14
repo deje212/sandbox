a049913158865f3c6ffd2f73f903359ce50d96d8
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2014-10-14 20:49:37 -0300

    Minor fix on cycle_counting.asm

--- src/cycle_counting.asm
+++ src/cycle_counting.asm
@@ -5,21 +5,15 @@ section .text
 ; unsigned long long start_cycle_count(void);
 global start_cycle_count:function
 start_cycle_count:
-  push rcx
-  push rdx
   times 16 rdtscp ; 16 rdtscp instructions for warm-up.
   shl rdx,32
   or  rax,rdx
-  pop rdx
-  pop rcx
   ret
 
 ; unsigned long long end_cycle_count(void);
 global end_cycle_count:function
 end_cycle_count:
-  push rdx
   rdtsc
   shl rdx,32
   or  rax,rdx
-  pop rdx
   ret
